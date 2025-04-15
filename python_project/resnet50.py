d6aef70398cd84d3775e64bdeecab41dc2b1f702
Pulkit Chouhan <chouhan.p@icloud.com>
2025-04-15 20:50:44 +0200

    s

--- python_project/resnet50.py
+++ python_project/resnet50.py
@@ -3,6 +3,11 @@ import torch.nn as nn
 import torch.optim as optim
 from train_evaluate import train_and_evaluate
 import torch
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
 
 #Define hyperparameters
 batch_size = 32
@@ -24,3 +29,15 @@ def train_and_eval(train_dataset, test_dataset, weights, resnet_name='NoNameResn
     return train_and_evaluate(
         model, optomizer, criterion, device, train_dataset, test_dataset, num_epochs, batch_size, resnet_name,
     )
+    
+
+# Function to create a ResNet50 model
+def create_resnet50(weights, num_classes):
+    print(f"Creating ResNet50 model with weights: {weights}")
+    base_model = ResNet50(weights=weights, include_top=False, input_shape=(224, 224, 3))
+    x = base_model.output
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(1024, activation="relu")(x)
+    predictions = Dense(num_classes, activation="softmax")(x)
+    model = Model(inputs=base_model.input, outputs=predictions)
+    return model
