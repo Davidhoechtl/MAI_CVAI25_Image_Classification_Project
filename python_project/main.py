9acfef987d57e05c95a72a084fdb06993fd2fc61
Davidhoechtl <davidhoechtl.job@gmail.com>
2025-04-18 23:08:18 +0200

    cleaned code and added results pictures and powerpoint

--- python_project/main.py
+++ python_project/main.py
@@ -1,18 +1,14 @@
 from pathlib import Path
+from tensorflow.python.ops.confusion_matrix import confusion_matrix
 import downloader
 from tensorflow import keras as ks
 import tensorflow as tf
-from keras.applications import ResNet50
-from keras.layers import Dense, GlobalAveragePooling2D
-from keras.models import Model
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from keras.optimizers import Adam
 import time
 import numpy as np
 import matplotlib.pyplot as plt
 from augmentaion import apply_augmentation_to_set
-from activaion_map import preprocess_image, generate_activation_map, show_activation_map
-import os
+from activaion_map import show_activation_map
 import ResNet50Factory
 import pandas as pd
 
@@ -20,45 +16,56 @@ print(tf.__version__)
 print(tf.test.is_built_with_cuda())
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
-epochs = 10
-
-# Download data if it doesn't exist
-if downloader.data_exists() == False:
-    downloader.download_images()
+epochs = 1
 
 # Define paths for data
 workspaceFold = Path(__file__).resolve().parent.parent
 train_folder = workspaceFold / "data" / "train"
 test_folder = workspaceFold / "data" / "test"
 augmented_data_output_folder = workspaceFold / "data" / "train_augmented"
+# combined_folder = workspaceFold / "data" / "train_combined"
+# cleaned_folder = workspaceFold / "data" / "train_cleaned"
+# test_cleaned_folder = workspaceFold / "data" / "test_cleaned"
 # Define the folder containing the images
 images_folder_for_activation_map = workspaceFold / "python_project/images_for_actmap"
 
-# Apply augmentations to the dataset
-apply_augmentation_to_set(train_folder/"Axe", augmented_data_output_folder/"Axe")
-apply_augmentation_to_set(train_folder/"Box", augmented_data_output_folder/"Box")
-apply_augmentation_to_set(train_folder/"Briefcase", augmented_data_output_folder/"Briefcase")
+# Download data if it doesn't exist
+if downloader.data_exists() == False:
+    downloader.download_images()
+
+    # Apply augmentations to the dataset
+    apply_augmentation_to_set(train_folder/"Axe", augmented_data_output_folder/"Axe")
+    apply_augmentation_to_set(train_folder/"Box", augmented_data_output_folder/"Box")
+    apply_augmentation_to_set(train_folder/"Briefcase", augmented_data_output_folder/"Briefcase")
 
 # Data augmentation and preprocessing
-train_datagen = ks.preprocessing.image.ImageDataGenerator(rescale=1.0 / 255)
-test_datagen = ks.preprocessing.image.ImageDataGenerator(rescale=1.0 / 255)
+image_pipeline = ks.preprocessing.image.ImageDataGenerator(rescale=1.0 / 255)
 
 # Load datasets
-train_generator = train_datagen.flow_from_directory(
+train_generator = image_pipeline.flow_from_directory(
     train_folder, target_size=(224, 224), batch_size=32, class_mode="categorical"
 )
-test_generator = test_datagen.flow_from_directory(
+test_generator = image_pipeline.flow_from_directory(
     test_folder, target_size=(224, 224), batch_size=32, class_mode="categorical"
 )
-train_generator_augmented = train_datagen.flow_from_directory(
+train_generator_augmented = image_pipeline.flow_from_directory(
     augmented_data_output_folder, target_size=(224, 224), batch_size=32, class_mode="categorical"
 )
+# train_generator_combined = image_pipeline.flow_from_directory(
+#     combined_folder, target_size=(224, 224), batch_size=32, class_mode="categorical"
+# )
+# train_generator_cleaned= image_pipeline.flow_from_directory(
+#     cleaned_folder, target_size=(224, 224), batch_size=32, class_mode="categorical"
+# )
+# test_cleaned_folder_generator_cleaned= image_pipeline.flow_from_directory(
+#     test_cleaned_folder, target_size=(224, 224), batch_size=32, class_mode="categorical"
+# )
 
 history_dict  = {}
 def evaluate_model(train_gen, test_gen, model):
     print(f"{model.name} start...")
     model_start_time = time.time()
-    model.compile(optimizer=Adam(learning_rate=0.001), loss="categorical_crossentropy", metrics=["accuracy"])
+    model.compile(optimizer=Adam(learning_rate=0.002), loss="categorical_crossentropy", metrics=["accuracy"])
     model_history = model.fit(train_gen, epochs=epochs, validation_data=test_gen)
     model_end_time = time.time()
     print(f"{model.name} Runtime: {model_end_time - model_start_time:.2f} seconds")
@@ -72,10 +79,60 @@ overall_start_time = time.time()
 plain_model = ResNet50Factory.create_resnet50(weights=None, num_classes=train_generator.num_classes, name="Plain_ResNet50")
 evaluate_model(train_generator, test_generator, plain_model)
 
+# evaluate confusion matrix for plain resnet50
+y_pred_probs = plain_model.predict(test_generator)
+y_pred = np.argmax(y_pred_probs, axis=1)
+y_true = test_generator.classes
+cm_tensor = confusion_matrix(y_true, y_pred)
+cm = cm_tensor.numpy()
+class_labels = list(test_generator.class_indices.keys())
+plt.figure(figsize=(8, 6))
+plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+plt.title('Confusion Matrix')
+plt.colorbar()
+tick_marks = np.arange(len(class_labels))
+plt.xticks(tick_marks, class_labels, rotation=45)
+plt.yticks(tick_marks, class_labels)
+thresh = cm.max() / 2.0
+for i in range(cm.shape[0]):
+    for j in range(cm.shape[1]):
+        plt.text(j, i, format(cm[i, j], 'd'),
+                 ha="center", va="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+plt.ylabel('True Label')
+plt.xlabel('Predicted Label')
+plt.tight_layout()
+plt.show()
+
 # Experiment 2 - Pre-trained ResNet50 (Transfer Learning)
 pretrained_model = ResNet50Factory.create_resnet50(weights="imagenet", num_classes=train_generator.num_classes, name="Pre_trained_ResNet50")
 evaluate_model(train_generator, test_generator, pretrained_model)
+# evaluate confusion matrix for plain resnet50
+y_pred_probs = pretrained_model.predict(test_generator)
+y_pred = np.argmax(y_pred_probs, axis=1)
+y_true = test_generator.classes
+cm_tensor = confusion_matrix(y_true, y_pred)
+cm = cm_tensor.numpy()
+class_labels = list(test_generator.class_indices.keys())
+plt.figure(figsize=(8, 6))
+plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+plt.title('Confusion Matrix')
+plt.colorbar()
+tick_marks = np.arange(len(class_labels))
+plt.xticks(tick_marks, class_labels, rotation=45)
+plt.yticks(tick_marks, class_labels)
+thresh = cm.max() / 2.0
+for i in range(cm.shape[0]):
+    for j in range(cm.shape[1]):
+        plt.text(j, i, format(cm[i, j], 'd'),
+                 ha="center", va="center",
+                 color="white" if cm[i, j] > thresh else "black")
 
+plt.ylabel('True Label')
+plt.xlabel('Predicted Label')
+plt.tight_layout()
+plt.show()
 # Experiment 3 - Plain ResNet50 with Augmented Data
 plain_aug_model = ResNet50Factory.create_resnet50(weights=None, num_classes=train_generator_augmented.num_classes, name="plain_aug_model")
 evaluate_model(train_generator_augmented, test_generator, plain_aug_model)
@@ -148,30 +205,6 @@ plt.legend(title="Model", bbox_to_anchor=(1.05, 1), loc='upper left')
 plt.grid(True)
 plt.tight_layout()
 plt.show()
-
-# Comparisons
-# print("\nComparison of Plain vs Pre-trained ResNet50:")
-# for epoch in range(10):
-#     print(f"Epoch {epoch+1}: Plain Loss = {plain_history.history['loss'][epoch]:.4f}, "
-#           f"Pre-trained Loss = {pretrained_history.history['loss'][epoch]:.4f}, "
-#           f"Plain Accuracy = {plain_history.history['accuracy'][epoch]:.2f}, "
-#           f"Pre-trained Accuracy = {pretrained_history.history['accuracy'][epoch]:.2f}")
-#
-# print("\nComparison of Plain vs Pre-trained ResNet50 (Augmented Data):")
-# for epoch in range(10):
-#     print(f"Epoch {epoch+1}: Plain Augmented Loss = {plain_aug_history.history['loss'][epoch]:.4f}, "
-#           f"Pre-trained Augmented Loss = {pretrained_aug_history.history['loss'][epoch]:.4f}, "
-#           f"Plain Augmented Accuracy = {plain_aug_history.history['accuracy'][epoch]:.2f}, "
-#           f"Pre-trained Augmented Accuracy = {pretrained_aug_history.history['accuracy'][epoch]:.2f}")
-#
-# print("\nComparison of Augmented vs Non-Augmented Results:")
-# for epoch in range(10):
-#     print(f"Epoch {epoch+1}: Non-Augmented Loss = {plain_history.history['loss'][epoch]:.4f}, "
-#           f"Augmented Loss = {plain_aug_history.history['loss'][epoch]:.4f}, "
-#           f"Non-Augmented Accuracy = {plain_history.history['accuracy'][epoch]:.2f}, "
-#           f"Augmented Accuracy = {plain_aug_history.history['accuracy'][epoch]:.2f}")
-    
-
 
 #Classify images and generate activation maps for each model
 models = [
